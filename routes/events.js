5db84bf8a5675e9a8c6a402de8e3cb22e02df9d5
lancevd <olamosob@gmail.com>
2024-07-24 16:02:43 +0100

    user model

--- routes/events.js
+++ routes/events.js
@@ -17,3 +17,37 @@ router.put("/:id", updateEvent);
 router.delete("/:id", deleteEvent);
 
 export default router;
+
+
+// import express from "express";
+// import multer from "multer";
+// import {
+//   getEvents,
+//   createEvent,
+//   getEventById,
+//   updateEvent,
+//   deleteEvent,
+// } from "../controllers/eventController.js";
+
+// const router = express.Router();
+
+// // Set up multer for file uploads
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// // Routes
+// router.get("/", getEvents);
+// router.get("/:id", getEventById);
+// router.post("/", upload.single("image"), createEvent);
+// router.put("/:id", upload.single("image"), updateEvent);
+// router.delete("/:id", deleteEvent);
+
+// export default router;
